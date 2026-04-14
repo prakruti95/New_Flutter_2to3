528c3656e38231fac6008f14f652b91ca90bb375
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2026-04-14 14:49:17 +0530

    commit

---
+++ Module2/14-4/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:myapp123/profilescreen.dart';
+
+void main()
+{
+  runApp(TopsApp());
+}
+
+class TopsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: FirstScreen());
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome to Tops"),
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+
+        children: [
+          Center
+            (
+            child: Text(
+              "Company List",
+              style: TextStyle(
+                fontSize: 20.00,
+                color: Colors.lightBlue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ProfileScreen()
+        ],
+      ),
+    );
+  }
+}
